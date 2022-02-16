f606ca64d69b724fd277fd13509ad5fc8c152a71
Micky McKeon <micky.mckeon@willowtreeapps.com>
2022-02-16 10:33:04 -0500

    Initial Commit

---
+++ Selfiegram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Micky McKeon on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
